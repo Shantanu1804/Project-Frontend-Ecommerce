01a070b8511e0aef9bcbcc1f6bb300cc513c5f5e
Shantanu <shantanu.sharan10@yahoo.com>
2022-09-28 02:15:24 +0530

    Commit Alibaba

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Header = () => {
+  return (
+    <div className="banner">
+      <Navbar/>
+      <div className = "banner__content">
+        <div className ="container">
+          <div className ="banner__text">
+          <h3>Vocal for Local</h3>
+          <h1>HANDICRAFT PRODUCTS</h1>
+          <p>HANDifdfg f dfkjvbdfg fvbdcvkdbfvdf </p>
+          </div>
+          
+
+        </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
